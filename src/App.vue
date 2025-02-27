78a61ab08d680ee7d16280f4b9a2037f6a75de43
kVaZzzz <elokhov.2015@mail.ru>
2025-02-27 14:42:43 +0700

    Footer

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 <main>
   <AppHeader></AppHeader>
   <AppMainPage></AppMainPage>
+  <AppFooter></AppFooter>
 </main>
 </template>
 
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,8 +1,75 @@
 <template>
+  <div class="img_footer">
+    <div>
+      <img src="./icons/TOMSK.svg">
+    </div>
+  </div>
+  <div class="footer">
+    <div class="nav">
+      <div class="links_footer">
+      <a>Длительная аренда</a>
+      <a>Недвижимость посуточно</a>
+      <a>Политика конфиденциальности</a>
+      </div>
+      <div class="icons">
+        <img src="./icons/icon_telegram.svg">
+        <img src="./icons/icon_vk.svg">
+      </div>
+    </div>
+    <div class="footer_contacts">
+      <div class="footer_email">TomskZone@mail.com</div>
+      <div class="footer_phone">8 (800) 111-11-11</div>
+    </div>
+    <div class="idk">© 2011—2025 TomskZone</div>
+  </div>
 </template>
 
 <script setup>
 
 </script>
 
-<style></style>
+<style>
+.img_footer{
+  margin-left: 160px;
+  margin-right: 160px;
+  background-color: black;
+}
+.footer{
+  margin-left: 160px;
+  margin-right: 160px;
+  width: 1600px;
+  height: 217px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.nav{
+  display: flex;
+  justify-content: space-between;
+  margin-left: 475px;
+  margin-right: 24px;
+  margin-top: 1%;
+}
+.links_footer{
+  display: flex;
+  justify-content: space-around;
+  width: 700px;
+  font-size: 16px;
+  margin-left: -2%;
+}
+.icons>img:nth-child(2){
+  margin-left: 20px;
+}
+.footer_contacts{
+  font-size: 45px;
+  display: flex;
+  justify-content: space-between;
+}
+.idk{
+  display: flex;
+  justify-content: center;
+  font-size: 16px;
+  margin-bottom: 16px;
+}
+</style>
